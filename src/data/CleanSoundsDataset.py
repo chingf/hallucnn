4682607a8fa846a2fccf3fa9ab3d8879da3242fd
es3773 <es3773@axon.rc.zi.columbia.edu>
2022-06-14 11:52:53 -0400

    Add dataset class for Psychophysics clean dataset

--- src/data/CleanSoundsDataset.py
+++ src/data/CleanSoundsDataset.py
@@ -29,3 +29,36 @@ class CleanSoundsDataset(Dataset):
             idx = idx.tolist()
         return self.data[idx], self.labels[idx]
 
+class PsychophysicsCleanSoundsDataset(Dataset):
+    """ Corresponding clean sounds to Psychophysics dataset """
+
+    def __init__(self, clean_in, labels, orig_dset, exclude_timit=True, subset=None):
+        if exclude_timit:
+            new_clean_in = []
+            for idx, orig_dset in enumerate(orig_dset):
+                if orig_dset == 'WSJ':
+                    new_clean_in.append(clean_in[idx])
+            new_clean_in = np.array(new_clean_in)
+            labels = labels[orig_dset=='WSJ']
+            clean_in = new_clean_in
+        n_samples = clean_in.shape[0]
+        shuffle_idxs = np.arange(n_samples)
+        np.random.shuffle(shuffle_idxs)
+        clean_in = clean_in[shuffle_idxs]
+        labels = labels.squeeze()
+        self.labels = labels[shuffle_idxs]
+        self.data = torch.tensor(
+            clean_in.reshape((n_samples, 1, 164, 400))
+            )
+        if subset is not None:
+            self.data = self.data[:subset]
+            self.labels = self.labels[:subset]
+        self.n_data, self.n_channels, self.height, self.width = self.data.shape
+
+    def __len__(self):
+        return self.n_data
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        return self.data[idx], self.labels[idx]
