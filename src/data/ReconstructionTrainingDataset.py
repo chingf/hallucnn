ef9b78bade6f25cd6a8d844e10b667f3dec9e4b4
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-01-12 10:09:22 -0500

    Add cgram shuffle option into reconstruction dataset

--- src/data/ReconstructionTrainingDataset.py
+++ src/data/ReconstructionTrainingDataset.py
@@ -8,15 +8,28 @@ class CleanSoundsDataset(Dataset):
     """
     Clean sounds dataset from WSJ, but excludes the psychophysics.
     HDF_FILE is typically: {engram_dir}clean_reconstruction_training_set
+
+    cgram_shuffle: 0: None, 1: frequency shuffle, 2: temporal shuffle
     """
 
-    def __init__(self, hdf_file, subset=None, train=True, label_key='label_indices', scaling=1000):
+    def __init__(
+        self, hdf_file, subset=None, train=True, label_key='label_indices',
+        scaling=1000, cgram_shuffle=0
+        ):
         self.hdf_file = hdf_file
         self.label_key = label_key
         self.scaling = scaling
         self.train = train
+        self.cgram_shuffle = cgram_shuffle
         self.f = h5py.File(hdf_file, 'r')
         self.n_data, __ =  np.shape(self.f['data'])
+
+        if self.cgram_shuffle == 1:
+            self.shuffle_idxs = np.zeros((self.n_data, 164), dtype=int)
+            for i in range(self.n_data):
+                indices = np.arange(164, dtype=int)
+                np.random.shuffle(indices)
+                self.shuffle_idxs[i] = indices
         
         if subset is not None:
             if train: 
@@ -36,6 +49,13 @@ class CleanSoundsDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         item = np.array(self.f['data'][idx]).reshape((-1, 164, 400))*self.scaling
+        if self.cgram_shuffle == 1:
+            shuffle_idxs = self.shuffle_idxs[idx]
+            if item.shape[0] == 1:
+                item = item[:, shuffle_idxs]
+            else:
+                for batch in range(shuffle_idxs.shape[0]):
+                    item[batch] = item[batch, shuffle_idxs[batch]]
         label = self.f[self.label_key][idx]
         return torch.tensor(item), torch.tensor(label).type(torch.LongTensor)
  
