54916eb3daf8c188e0e99e68599eef2599865a7d
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-05-14 06:30:31 -0400

    Add notebook to fit PCA model for each bg/snr

--- scripts/06_fit_PCA.py
+++ scripts/06_fit_PCA.py
@@ -24,56 +24,39 @@ bg_types = ['pinkNoise', 'AudScene', 'Babble8Spkr']
 snr_types = [-9.0, -6.0, -3.0, 0.0, 3.0]
 
 ##### HELPER FUNCTIONS #####
-def get_data(conv_idx, t, sample_size=None):
-    X = []
-    bgs = []
-    snrs = []
-    dset_idxs = []
-    n_total_data = 0
-    n_sampled_data = 0
+def get_data_and_fit_PCA(conv_idx, t, pca_activations_dir):
     for bg in bg_types:
         for snr in snr_types:
             activ_dir = f'{activations_dir}{bg}_snr{int(snr)}/'
             for results_file in os.listdir(activ_dir):
                 results_filepath = f'{activ_dir}{results_file}'
                 results = h5py.File(results_filepath, 'r')
+            break
             if conv_idx > 3:
                 activ = np.array(results[f'conv{conv_idx}_W_{t}_activations'])
             else:
                 activ = np.array(results[f'conv{conv_idx}_{t}_activations'])
             n_data = activ.shape[0]
-            n_total_data += n_data
-            if sample_size != None:
-                sample_idxs = np.random.choice(n_data, size=sample_size)
-                activ = activ[sample_idxs]
-                n_sampled_data += sample_size
-                _dset_idxs = list(sample_idxs)
-            else:
-                n_sampled_data += n_data
-                _dset_idxs = list(range(n_data))
-            new_n_data = activ.shape[0]
-            activ = list(activ.reshape((new_n_data, -1)))
-            X.extend(activ)
-            snrs.extend([snr]*new_n_data)
-            bgs.extend([bg]*new_n_data)
-            dset_idxs.extend(_dset_idxs)
-            
-            del results
-            del activ
-            gc.collect()
-
-    idxs = np.arange(len(X))
-    np.random.shuffle(idxs)
-
-    X = np.array(X)[idxs]
-    bgs = np.array(bgs)[idxs]
-    snrs = np.array(snrs)[idxs]
-    dset_idxs = np.array(dset_idxs)[idxs]
-    
-    print(f'Sampled {n_sampled_data}/{n_total_data} data')
-    print(f'with {X.shape[1]} features')
-    
-    return X, bgs, snrs, dset_idxs
+            activ = activ.reshape((n_data, -1))
+            print(f'Runing PCA on {bg} {snr} with data shape {activ.shape}')
+            pca = PCA()
+            pca.fit(activ)
+            pca_filename = f'PCA_{bg}_{snr}_conv{conv_idx}_t{t}'
+            with open(f'{pca_activations_dir}{pca_filename}.p', 'wb') as f:
+                pickle.dump(pca, f, protocol=4)
+    # Repeat for clean
+    if conv_idx > 3:
+        activ = np.array(results[f'conv{conv_idx}_W_{t}_clean_activations'])
+    else:
+        activ = np.array(results[f'conv{conv_idx}_{t}_clean_activations'])
+    n_data = activ.shape[0]
+    activ = activ.reshape((n_data, -1))
+    print(f'Runing PCA on clean with data shape {activ.shape}')
+    pca = PCA()
+    pca.fit(activ)
+    pca_filename = f'PCA_clean_conv{conv_idx}_t{t}'
+    with open(f'{pca_activations_dir}{pca_filename}.p', 'wb') as f:
+        pickle.dump(pca, f, protocol=4)   
 
 def get_cpu_usage():
     total_memory, used_memory, free_memory = map(
@@ -83,28 +66,13 @@ def get_cpu_usage():
     p_used = round((used_memory/total_memory) * 100, 2)
     print(f"RAM {used_memory} GB, {p_used}% used")
 
-def fit_and_save(conv_idx, t):
-    X, bgs, snrs, dset_idxs = get_data(conv_idx, t)
-    print('Fitting Model...')
-    pca = PCA()
-    pca.fit(X)
-    print(f'{pca.n_components_} components used for % variance explained:')
-    print(np.sum(pca.explained_variance_ratio_))
-    get_cpu_usage()
-
-
-    os.makedirs(pca_activations_dir, exist_ok=True)
-    pca_filename = f'FullPCAmodel_conv{conv_idx}_t{t}'
-    with open(f'{pca_activations_dir}{pca_filename}.p', 'wb') as f:
-        pickle.dump(pca, f, protocol=4)
-
 ##### MAIN CALL #####
 
 if __name__ == "__main__":
+    os.makedirs(pca_activations_dir, exist_ok=True)
     for conv_idx in [1, 2, 3, 4, 5]:
         for t in [0, 1, 2, 3, 4]:
-            if conv_idx==1 and t==1: continue
             print(f'====== PROCESSING LAYER {conv_idx}, TIMESTEP {t} ======')
-            fit_and_save(conv_idx, t)
+            get_data_and_fit_PCA(conv_idx, t, pca_activations_dir)
             gc.collect()
 
