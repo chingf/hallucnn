25ffc10ed5d929cb340a4664becab73492386257
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-06-08 16:05:18 -0400

    Move noise factorization calc into script with options for AUC and shuffle

--- scripts/08_calc_factorization.py
+++ scripts/08_calc_factorization.py
@@ -17,6 +17,8 @@ from data.ValidationDataset import NoisyDataset
 
 # Arguments
 netname = str(sys.argv[1]) # pnet
+shuffle = True
+auc = True
 engram_dir = '/mnt/smb/locker/abbott-locker/hcnn/'
 activations_dir = f'{engram_dir}3_activations/{netname}/'
 pickles_dir = f'{engram_dir}pickles/'
@@ -46,7 +48,35 @@ def get_data(conv_idx, t, bg, snr):
     
     return activ, clean_activ, np.array(results['label'])
 
+def get_projection(activ, pca):
+    activ_centered = activ - pca.mean_[None,:]
+    projected_activ = activ_centered @ (pca.components_).T
+    return projected_activ
+
+def get_explained_var(activ, pca, auc=True):
+    """ ACTIV should be of shape (N, DIMS)"""
+    
+    cum_var = np.cumsum(pca.explained_variance_ratio_)
+    K = np.argwhere(cum_var>0.9)[0].item()
+    cum_var_K = cum_var[K]
+    activ_centered = activ - pca.mean_[None,:]
+    sample_size = activ.shape[0]
+    if sample_size == 0: sample_size = 1
+    total_var = np.sum(np.square(activ_centered))/sample_size
+    projected_activ = activ_centered @ pca.components_.T
+    sample_size = projected_activ.shape[0]-1
+    if sample_size == 0: sample_size = 1
+    explained_var = np.sum(np.square(projected_activ), axis=0)/sample_size
+    explained_var = explained_var/total_var
+    if auc:
+        var_curve = np.cumsum(explained_var)
+        clean_explained = np.trapz(var_curve, dx=1/var_curve.size)
+    else:
+        clean_explained = np.sum(explained_var[:K+1])
+    return clean_explained
+
 if __name__ == "__main__":
+    # Measure factorization for each noise/layer/timestep
     bgs = []
     snrs = []
     convs = []
@@ -56,40 +86,58 @@ if __name__ == "__main__":
         for snr in snr_types:
             for conv_idx in [1,2,3,4,5]:
                 for t in [0,1,2,3,4]:
-                    activ, clean_activ, label = get_data(conv_idx, t, bg, snr)
-                    bgs.append(bg)
-                    snrs.append(snr)
-                    convs.append(conv_idx)
-                    ts.append(t)
+                    print(f'{bg}, {snr}, conv {conv_idx}, t {t}')
                     
-                    clean_pca_path = f'{pca_activations_dir}PCA_clean_conv{conv_idx}_t{t}.p'
+                    # Load data and PCA model
+                    activ, clean_activ, label = get_data(conv_idx, t, bg, snr)
+                    print(activ.shape)
+
+                    if not shuffle:
+                        clean_pca_path = f'{pca_activations_dir}PCA_{bg}_{snr}_clean_conv{conv_idx}_t{t}.p'
+                    else:
+                        # Dummy clean dataset-- actually a shuffle
+                        clean_pca_path = f'{pca_activations_dir}PCA_{bg}_{snr}_shufflehalf_conv{conv_idx}_t{t}.p'
+                        _activ = np.zeros(activ.shape) # Dummy noisy dataset
+                        _activ[1::2] = np.copy(clean_activ[1::2])
+                        _activ[::2] = np.copy(activ[::2])
+                        _clean_activ = np.zeros(clean_activ.shape) # Dummy clean
+                        _clean_activ[1::2] = np.copy(activ[1::2])
+                        _clean_activ[::2] = np.copy(clean_activ[::2])
+                        activ = _activ
+                        clean_activ = clean_activ
+
                     with open(clean_pca_path, 'rb') as f:
                         clean_pca = pickle.load(f)
-                    clean_cum_var = np.cumsum(clean_pca.explained_variance_ratio_)
-                    K = np.argwhere(clean_cum_var>0.9)[0].item()
-                    cum_var_K = clean_cum_var[K]
-                    activ_centered = activ - clean_pca.mean_[None,:]
-                    total_var = np.sum(np.square(activ_centered))/(activ.shape[0]-1)
-                    explained_variance = []
-                    for k in range(K+1):
-                        var = np.sum(np.square(
-                            activ_centered @ clean_pca.components_[k]
-                            ))/(activ.shape[0]-1)
-                        explained_variance.append(var)
-                    clean_explained = np.sum(explained_variance)/total_var
-                    _factorization = 1 - clean_explained/cum_var_K
-                    factorization.append(_factorization)
+                        
+                    # Calculate factorization ratio for each sample (vectorize!!)
+                    for i in range(activ.shape[0]):
+                        noise_var = get_explained_var(
+                            activ[i].reshape((1,-1)), clean_pca, auc=auc)
+                        clean_var = get_explained_var(
+                            clean_activ[i].reshape((1,-1)), clean_pca, auc=auc)
+                        bgs.append(bg)
+                        snrs.append(snr)
+                        convs.append(conv_idx)
+                        ts.append(t)
+                        factorization.append(noise_var/clean_var)
 
-    factorization_df = pd.DataFrame({
+                    del clean_pca
+                    gc.collect()
+            
+    df = pd.DataFrame({
         'BG': bgs,
         'SNR': snrs,
         'Conv': convs,
         'T': ts,
-        'Factorization': factorization,
+        'Factorization': factorization
         })
-
     os.makedirs(pickles_dir, exist_ok=True)
-    pfile = f'{pickles_dir}{netname}_factorization.p'
+    pfile = 'factorization.p'
+    if auc:
+        pfile = 'auc_' + pfile
+    if shuffle:
+        pfile = 'shuffle_' + pfile
+    pfile = f'{pickles_dir}{netname}_{pfile}'
     with open(pfile, 'wb') as f:
-        pickle.dump(factorization_df, f)
+        pickle.dump(df, f)
 
