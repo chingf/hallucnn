3a3f73ea76e6ade375acd72c6fd2612bdd22e6a7
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-07-06 16:51:26 -0400

    New invariance equation following lindsey 2023

--- scripts/08_calc_invariance.py
+++ scripts/08_calc_invariance.py
@@ -17,99 +17,98 @@ from data.ValidationDataset import NoisyDataset
 
 # Arguments
 netname = str(sys.argv[1]) # pnet
+if len(sys.argv) > 2:
+    shuffle_seed = int(sys.argv[2])
+    print(f'Shuffle-label invariance with seed {shuffle_seed}')
+    shuffle = True
+else:
+    shuffle = False
 engram_dir = '/mnt/smb/locker/abbott-locker/hcnn/'
-activations_dir = f'{engram_dir}3_activations/{netname}/'
+train_activations_dir = f'{engram_dir}3_train_activations/{netname}/'
+validation_activations_dir = f'{engram_dir}3_validation_activations/{netname}/'
 pickles_dir = f'{engram_dir}pickles/'
-pca_activations_dir = f'{engram_dir}4_activations_pca/{netname}/'
 bg_types = ['pinkNoise', 'AudScene', 'Babble8Spkr']
 snr_types = [-9.0, -6.0, -3.0, 0.0, 3.0]
 
 # Helper functions
-def get_data(conv_idx, t, bg, snr):
-    activ_dir = f'{activations_dir}{bg}_snr{int(snr)}/'
-    for results_file in os.listdir(activ_dir):
-        results_filepath = f'{activ_dir}{results_file}'
-        results = h5py.File(results_filepath, 'r')
-    if conv_idx > 3:
-        activ = np.array(results[f'conv{conv_idx}_W_{t}_activations'])
-    else:
-        activ = np.array(results[f'conv{conv_idx}_{t}_activations'])
-    n_data = activ.shape[0]
-    activ = activ.reshape((n_data, -1))
-    
-    # Repeat for clean
-    if conv_idx > 3:
-        clean_activ = np.array(results[f'conv{conv_idx}_W_{t}_clean_activations'])
-    else:
-        clean_activ = np.array(results[f'conv{conv_idx}_{t}_clean_activations'])
-    clean_activ = clean_activ.reshape((n_data, -1))
-    
-    return activ, clean_activ, np.array(results['label'])
+n_units_per_layer = {
+    1: (96, 55, 134), 2: (256, 14, 34),
+    3: (512, 7, 17), 4: (1024, 7, 17),
+    5: (512, 7, 17)
+    }
+n_labels = 531 # Label 0 is for negative samples; should not be used
 
-if __name__ == "__main__":
-    bgs = []
-    snrs = []
+def get_cpu_usage():
+    total_memory, used_memory, free_memory = map(
+        int, os.popen('free -t --giga').readlines()[-1].split()[1:])
+
+    # Memory usage
+    p_used = round((used_memory/total_memory) * 100, 2)
+    print(f"RAM {used_memory} GB, {p_used}% used")
+
+def get_data(conv_idx, t, shuffle=False):
+    """
+    Will return a (N, K, D) activity dataset.
+        N: number of utterances
+        K: augmentations of each utterance (noise)
+        D: number of units, flattened
+    """
+
+    N = 5000 # number of unique utterances in the hyperparameter dataset
+    activity = [[] for _ in range(N)]
+    for bg in bg_types:
+        for snr in snr_types:
+            activ_dir = f'{train_activations_dir}{bg}_snr{int(snr)}/'
+            for results_file in os.listdir(activ_dir):
+                if 'pt' not in results_file: continue
+                results_filepath = f'{activ_dir}{results_file}'
+                with h5py.File(results_filepath, 'r') as results:
+                    if conv_idx > 3:
+                        activ = np.array(results[f'conv{conv_idx}_W_{t}_activations'])
+                    else:
+                        activ = np.array(results[f'conv{conv_idx}_{t}_activations'])
+                    clean_index = np.array(results['clean_index'])
+                    if shuffle:
+                        np.random.seed(shuffle_seed)
+                        np.random.shuffle(clean_index)
+                    for _dset_idx, _clean_index in enumerate(clean_index):
+                        activity[_clean_index].append(activ[_dset_idx].flatten())
+    if shuffle:
+        np.random.seed()
+    return activity
+
+def main():
+    # Measure factorization for each noise/layer/timestep
     convs = []
     ts = []
-    clean_noise_dist = []
-    utterance_dist = []
-    clean_clean_dist = []
-    for bg in bg_types:
-        print(bg)
-        for snr in snr_types:
-            print(snr)
-            for conv_idx in [1,2,3,4,5]:
-                for t in [0,1,2,3,4]:
-                    print(f'Layer {conv_idx}, time {t}')
-                    activ, clean_activ, label = get_data(conv_idx, t, bg, snr)
-                    bgs.append(bg)
-                    snrs.append(snr)
-                    convs.append(conv_idx)
-                    ts.append(t)
-                    
-                    # Clean-noisy distance for same audio clip
-                    _clean_noise_dist = []
-                    for idx in range(len(activ)):
-                        _clean_noise_dist.append(
-                            np.square(activ[idx] - clean_activ[idx]))
-                    _clean_noise_dist = np.mean(_clean_noise_dist, axis=0)
-                    _clean_noise_dist = np.sum(_clean_noise_dist)
-                    clean_noise_dist.append(_clean_noise_dist)
-                    
-                    # Distance between utterances of the same word
-                    _utterance_dist = []
-                    for l in np.unique(label):
-                        utterance_activ = clean_activ[label==l]
-                        for i in range(utterance_activ.shape[0]):
-                            for j in range(0,i):
-                                _utterance_dist.append(np.square(
-                                    utterance_activ[i] - utterance_activ[j]))
-                    _utterance_dist = np.mean(_utterance_dist, axis=0)
-                    _utterance_dist = np.sum(_utterance_dist)
-                    utterance_dist.append(_utterance_dist)
-                    
-                    # Distance between any two clean audio clips
-                    _clean_clean_dist = []
-                    for idx in range(len(activ)):
-                        rand_idx = np.random.choice(len(activ))
-                        _clean_clean_dist.append(np.square(
-                            clean_activ[idx] - clean_activ[rand_idx]))
-                    _clean_clean_dist = np.mean(_clean_clean_dist, axis=0)
-                    _clean_clean_dist = np.sum(_clean_clean_dist)
-                    clean_clean_dist.append(_clean_clean_dist)
-    
-    invariance_df = pd.DataFrame({
-        'BG': bgs,
-        'SNR': snrs,
+    invariance = []
+
+    for conv_idx in [1,2,3,4,5]:
+        # Iterate over timesteps of predictive processing
+        for t in [0,1,2,3,4]:
+            activity = get_data(conv_idx, t, shuffle=shuffle)
+            N, K, D = activity.shape
+            var_noise = np.var(np.mean(activity, axis=1), axis=0).sum()
+            var_all = np.var(activity.reshape((-1, D)), axis=0).sum()
+            convs.append(conv_idx)
+            ts.append(t)
+            invariance.append(var_noise/var_all)
+            del activity
+            gc.collect()
+            
+    df = pd.DataFrame({
         'Conv': convs,
         'T': ts,
-        'Dist': clean_noise_dist,
-        'Invariance by Utterance': np.array(clean_noise_dist)/np.array(utterance_dist),
-        'Invariance by Radius': np.array(clean_noise_dist)/np.array(clean_clean_dist),
+        'Invariance': invariance
         })
-
     os.makedirs(pickles_dir, exist_ok=True)
-    pfile = f'{pickles_dir}{netname}_invariance.p'
+    pfile = 'invariance.p'
+    if shuffle:
+        pfile = 'shuffle_' + pfile
+    pfile = f'{pickles_dir}{netname}_{pfile}'
     with open(pfile, 'wb') as f:
-        pickle.dump(invariance_df, f)
-    
+        pickle.dump(df, f)
+
+if __name__ == "__main__":
+    main()
+
