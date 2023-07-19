030b1d2b23a7fa9f6bfadba2b0ffd71857ce9cf4
cf2794 <cf2794@ax04.rc.zi.columbia.edu>
2023-07-19 13:16:24 -0400

    Add helper function in utils to get r2 score of reconstruction

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@
 
 from typing import Callable
 import torch
+import numpy as np
+from sklearn.metrics import r2_score
 
 def train_pcoders(
     net: torch.nn.Module, optimizer: torch.optim.Optimizer,
@@ -68,7 +70,7 @@ def train_pcoders(
                 (epoch-1) * len(train_loader) + batch_index)
         optimizer.zero_grad()
 
-
+@torch.no_grad()
 def eval_pcoders(
     net: torch.nn.Module, loss_function: Callable, epoch: int,
     eval_loader: torch.utils.data.DataLoader, device: str,
@@ -92,8 +94,7 @@ def eval_pcoders(
     for batch_index, (images, _) in enumerate(eval_loader):
         net.reset()
         images = images.to(device)
-        with torch.no_grad():
-            outputs = net(images)
+        outputs = net(images)
         for i in range(net.number_of_pcoders):
             if i == 0:
                 final_loss[i] += loss_function(net.pcoder1.prd, images).item()
@@ -119,3 +120,65 @@ def eval_pcoders(
     if writer is not None:
         writer.add_scalar(f"MSE Eval/Sum", loss_sum, epoch-1)
 
+@torch.no_grad()
+def eval_pcoders_r2(
+    net: torch.nn.Module,
+    eval_loader: torch.utils.data.DataLoader, device: str,
+    units_to_sample: list=[]):
+    r"""
+    Returns:
+        scores: (n_pcoders,) array; each element is the r2 value, averaged over
+        the D-dimensions of the pcoder.
+    """
+
+    net.eval()
+
+    ss_residuals = [None for l in range(net.number_of_pcoders)]
+    targets = [[] for l in range(net.number_of_pcoders)]
+    mean_target = [None for l in range(net.number_of_pcoders)]
+    n_images = 0
+    for batch_index, (images, _) in enumerate(eval_loader):
+        net.reset()
+        images = images.to(device)
+        outputs = net(images)
+        n_images += images.shape[0]
+        for l in range(net.number_of_pcoders):
+            if l == 0:
+                target = images.reshape(images.shape[0], -1)
+                prd = net.pcoder1.prd.reshape(images.shape[0], -1)
+            else:
+                pcoder_pre = getattr(net, f"pcoder{l}").rep
+                pcoder_curr = getattr(net, f"pcoder{l+1}").prd
+                target = pcoder_pre.reshape(pcoder_pre.shape[0], -1)
+                prd = pcoder_curr.reshape(pcoder_curr.shape[0], -1)
+
+            # Sample units if needed
+            if len(units_to_sample) > 0:
+                target = target[:, units_to_sample[l]]
+                prd = prd[:, units_to_sample[l]]
+
+            resid = target - prd
+            if ss_residuals[l] is None:
+                ss_residuals[l] = np.zeros(resid.shape[1])
+                mean_target[l] = np.zeros(resid.shape[1])
+            ss_residuals[l] += torch.sum(torch.square(resid), dim=0).cpu().numpy()
+            targets[l].extend([t.cpu().numpy() for t in target])
+            mean_target[l] += torch.sum(target, dim=0).cpu().numpy()
+
+    # Calculate mean_response
+    mean_target = [t/n_images for t in mean_target]
+
+    # Calculate sum-of-squares total
+    ss_total = []
+    for l in range(net.number_of_pcoders):
+        _targets = np.array(targets[l])
+        _mean_target = mean_target[l]
+        _ss_total = np.sum(np.square(_targets - _mean_target[None,:]), axis=0)
+        ss_total.append(_ss_total)
+
+    # Calculate r^2 score
+    scores = [ss_residuals[l] / ss_total[l] for l in range(net.number_of_pcoders)]
+    scores = [1-r for r in scores]
+    scores = [np.median(r) for r in scores]
+    return scores
+    
